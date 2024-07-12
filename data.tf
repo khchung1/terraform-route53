bb520ab06d7d254fcda9d68bb6145a9ce5c2f5d7
khchung1 <khchung87@gmail.com>
2024-07-12 22:39:39 +0800

    edit bucket policy & cloudfront

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "s3_public_read_policy" {
+data "aws_iam_policy_document" "s3_cloudfront_access" {
   statement {
     sid       = "PublicReadGetObject"
     actions   = ["s3:GetObject"]
@@ -6,12 +6,27 @@ data "aws_iam_policy_document" "s3_public_read_policy" {
     resources = ["arn:aws:s3:::${aws_s3_bucket.static_web.id}/*"] 
 
     principals {
-      type        = "*"
-      identifiers = ["*"]
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["${aws_cloudfront_distribution.s3_distribution.arn}"]
     }
   }
 }
 
 data "aws_route53_zone" "selected" {
   name         = "sctp-sandbox.com."
+}
+
+data "aws_cloudfront_cache_policy" "example" {
+  name = "Managed-CachingOptimized"
+}
+
+data "aws_acm_certificate" "name" {
+  provider = aws.us-east-1 #Syntax: <provider>.<alias>
+  domain   = "sctp-sandbox.com"
 }
--- main.tf
+++ main.tf
@@ -2,18 +2,18 @@ resource "aws_s3_bucket" "static_web" {
   bucket = "kokhui-staticwebsite.sctp-sandbox.com"
 }
 
-resource "aws_s3_bucket_public_access_block" "static_web_resource" {
-  bucket = aws_s3_bucket.static_web.id
+# resource "aws_s3_bucket_public_access_block" "static_web_resource" {
+#   bucket = aws_s3_bucket.static_web.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
-resource "aws_s3_bucket_policy" "static_web_bucket" {
+resource "aws_s3_bucket_policy" "s3_bucket_policy" {
   bucket = aws_s3_bucket.static_web.id
-  policy = data.aws_iam_policy_document.s3_public_read_policy.json
+  policy = data.aws_iam_policy_document.s3_cloudfront_access.json
 }
 
 resource "aws_s3_bucket_website_configuration" "static_web_config" {
@@ -40,12 +40,13 @@ resource "aws_route53_record" "www" {
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name              = "kokhui-staticwebsite.sctp-sandbox.com.s3.ap-southeast-1.amazonaws.com"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
     origin_id                = aws_s3_bucket.static_web.id
   }
 
   enabled             = true
   is_ipv6_enabled     = false
-  comment             = "Kok Hui CloudFront Follow-up"
+  comment             = "Kok Hui CloudFront Follow-up w OAC"
   default_root_object = "index.html"
 
 
@@ -54,77 +55,30 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = aws_s3_bucket.static_web.id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
+    cache_policy_id  = data.aws_cloudfront_cache_policy.example.id
     viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+
   }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = aws_s3_bucket.static_web.id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = aws_s3_bucket.static_web.id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  price_class = "PriceClass_200"
-
   restrictions {
     geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["SG"]
+      restriction_type = "none"
+      locations        = []
     }
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = data.aws_acm_certificate.name.arn
+    ssl_support_method  = "sni-only"
   }
 }
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "OAC for Website"
+  description                       = "OAC for S3 Static Website using CloudFront"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+
 
